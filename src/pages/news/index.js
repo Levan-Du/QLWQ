928c4fce35430770968e83af31111058ab407d61
LevanDD <441489115@qq.com>
2017-05-09 16:52:47 +0800

    news page

--- src/pages/news/index.js
+++ src/pages/news/index.js
@@ -1,1 +1,8 @@
-index.js
+import './index.css';
+import * from '../../commons/common.css';
+import snabbt from 'snabbt.js';
+import * from '../../commons/common';
+
+window.onload = (e) => {
+
+}
--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,12 @@ var webpack = require('webpack'),
 
 module.exports = {
     entry: {
-        index: path.join(__dirname, 'src/pages/index.js'),
-        games: path.join(__dirname, 'src/pages/index.js')
+        index: path.join(__dirname, 'src/pages/index/index.js'),
+        games: path.join(__dirname, 'src/pages/games/index.js'),
+        games: path.join(__dirname, 'src/pages/news/index.js'),
+        games: path.join(__dirname, 'src/pages/mall/index.js'),
+        games: path.join(__dirname, 'src/pages/user/index.js')
+
     },
     output: {
         path: path.join(__dirname, 'dist/js'),
@@ -21,14 +25,19 @@ module.exports = {
             test: /\.css$/,
             use: {
                 fallback: 'style-loader',
-                use: 'css-loader!postcss-loader'
+                use: 'css-loader'
             }
         }]
     },
     plugins: [
         new webpack.providePlugin({
             $: 'jquery'
+        }),        
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendors', // 将公共模块提取，生成名为`vendors`的chunk
+            minChunks: 2 // 提取至少3个模块共有的部分
         }),
+        new ExtractTextPlugin('css/[name].css?[contenthash]'),
         new HtmlWebpackPlugin({
             template: path.join(__dirname, 'src/pages/index/index.html'),
             filename: 'index.html',
